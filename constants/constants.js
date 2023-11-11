52e9d96f3a7f40cead8813b71ca87122f2f6b3e6
Алексей <87431805+OzornoyOleg@users.noreply.github.com>
2023-11-11 15:27:12 +0300

    Create constants.js

---
+++ constants/constants.js
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    title: 'Burger',
+    price: 1.99
+  },  
+  {
+    title: 'Pizza',
+    price: 1.50
+  },
+  {
+    title: 'Dunut',
+    price: 2.99
+  },  
+  {
+    title: 'Chicken',
+    price: 3.99
+  },
+  {
+    title: 'Cheeseburger',
+    price: 4.99
+  },  
+  {
+    title: 'Vopper',
+    price: 5.99
+  },
+]
